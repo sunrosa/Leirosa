d3ff7811b77cd28af14268d856a7f230b3581f1b
Ibis Sunrosa <drsunrosa@gmail.com>
2022-01-03 00:20:32 -0500

    Batch Gelbooru

--- NsfwModule.cs
+++ NsfwModule.cs
@@ -4,11 +4,31 @@ public class NsfwModule : Discord.Commands.ModuleBase<Discord.Commands.SocketCom
     [Discord.Commands.RequireNsfw]
     public async Task GelbooruAsync([Discord.Commands.Remainder]string tags_str = "")
     {
-        tags_str = tags_str.Replace(" ", "+");
+        tags_str = tags_str.Replace(" ", "+"); // Format tags for API request
 
         var client = new HttpClient(); // Create client for request
-        var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&json=1&tags=sort:random+{tags_str}"); /// Make request
+        var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&json=1&tags=sort:random+{tags_str}"); // Make request
         dynamic response_json = Newtonsoft.Json.JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()); // Parse request
         await ReplyAsync(response_json.post.file_url.ToString()); // Don't touch this sacred bullshit
+    }
+
+    [Discord.Commands.Command("mgelbooru")]
+    [Discord.Commands.RequireNsfw]
+    public async Task MGelbooruAsync([Discord.Commands.Remainder]string tags_str = "")
+    {
+        tags_str = tags_str.Replace(" ", "+"); // Format tags for API request
+        var count = 5;
+
+        var client = new HttpClient(); // Create client for request
+        var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit={count}&json=1&tags=sort:random+{tags_str}"); // Make request
+        dynamic response_json = Newtonsoft.Json.JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()); // Parse request
+
+        var output = "";
+        foreach (var post in response_json.post)
+        {
+            output += post.file_url.ToString() + "\n";
+        }
+
+        await ReplyAsync(output);
     }
 }
