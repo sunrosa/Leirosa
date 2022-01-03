e8f55b0e3fbc3953870479935d5c0e5a9bf7a693
Ibis Sunrosa <drsunrosa@gmail.com>
2022-01-03 17:47:42 -0500

    User friendly error message on file not found

--- NsfwModule.cs
+++ NsfwModule.cs
@@ -8,22 +8,30 @@ public class NsfwModule : Discord.Commands.ModuleBase<Discord.Commands.SocketCom
     [Discord.Commands.RequireContext(Discord.Commands.ContextType.DM, Group = "Private")]
     public async Task GelbooruAsync([Discord.Commands.Remainder]string tags_str = "")
     {
-        _log.Debug("\"gelbooru\" was called!");
+        try // Bad practice to wrap everything in a try-catch
+        {
+            _log.Debug("\"gelbooru\" was called!");
 
-        _log.Debug("Obtaining default tags...");
-        var default_tags = Program.config["default_gelbooru_tags"];
+            _log.Debug("Obtaining default tags...");
+            var default_tags = Program.config["default_gelbooru_tags"];
 
-        _log.Debug("Formatting requested tags...");
-        tags_str = tags_str.Replace(" ", "+"); // Format tags for API request
+            _log.Debug("Formatting requested tags...");
+            tags_str = tags_str.Replace(" ", "+"); // Format tags for API request
 
-        _log.Debug("Creating client for request...");
-        var client = new HttpClient(); // Create client for request
-        _log.Debug("Making request...");
-        var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&json=1&tags=sort:random+{default_tags}+{tags_str}"); // Make request
-        _log.Debug("Parsing request...");
-        dynamic response_json = Newtonsoft.Json.JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()); // Parse request
-        _log.Debug("Replying with file...");
-        await ReplyAsync(response_json.post[0].file_url.ToString(), messageReference: new Discord.MessageReference(Context.Message.Id)); // Don't touch this sacred bullshit
+            _log.Debug("Creating client for request...");
+            var client = new HttpClient(); // Create client for request
+            _log.Debug("Making request...");
+            var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&json=1&tags=sort:random+{default_tags}+{tags_str}"); // Make request
+            _log.Debug("Parsing request...");
+            dynamic response_json = Newtonsoft.Json.JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()); // Parse request
+            _log.Debug("Replying with file...");
+            await ReplyAsync(response_json.post[0].file_url.ToString(), messageReference: new Discord.MessageReference(Context.Message.Id)); // Don't touch this sacred bullshit
+        }
+        catch
+        {
+            _log.Debug("An exception was thrown. Replying with \"Not found.\"");
+            await ReplyAsync("Not found.");
+        }
     }
 
     [Discord.Commands.Command("mgelbooru")]
@@ -32,30 +40,38 @@ public class NsfwModule : Discord.Commands.ModuleBase<Discord.Commands.SocketCom
     [Discord.Commands.RequireContext(Discord.Commands.ContextType.DM, Group = "Private")]
     public async Task MGelbooruAsync([Discord.Commands.Remainder]string tags_str = "")
     {
-        _log.Debug("\"mgelbooru\" was called!");
-
-        _log.Debug("Obtaining default tags...");
-        var default_tags = Program.config["default_gelbooru_tags"];
-
-        _log.Debug("Formatting requested tags...");
-        tags_str = tags_str.Replace(" ", "+"); // Format tags for API request
-        var count = 5; // Discord only embeds 5 images per message. If we wanted to surpass this limit, we would have to have it auto-post into separate consecutive messages.
-
-        _log.Debug("Creating client for request...");
-        var client = new HttpClient(); // Create client for request
-        _log.Debug("Making request...");
-        var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit={count}&json=1&tags=sort:random+{default_tags}+{tags_str}"); // Make request
-        _log.Debug("Parsing request...");
-        dynamic response_json = Newtonsoft.Json.JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()); // Parse request
-
-        _log.Debug("Formatting output...");
-        var output = "";
-        foreach (var post in response_json.post)
+        try
         {
-            output += post.file_url.ToString() + "\n";
-        }
+            _log.Debug("\"mgelbooru\" was called!");
 
-        _log.Debug("Replying with files...");
-        await ReplyAsync(output, messageReference: new Discord.MessageReference(Context.Message.Id));
+            _log.Debug("Obtaining default tags...");
+            var default_tags = Program.config["default_gelbooru_tags"];
+
+            _log.Debug("Formatting requested tags...");
+            tags_str = tags_str.Replace(" ", "+"); // Format tags for API request
+            var count = 5; // Discord only embeds 5 images per message. If we wanted to surpass this limit, we would have to have it auto-post into separate consecutive messages.
+
+            _log.Debug("Creating client for request...");
+            var client = new HttpClient(); // Create client for request
+            _log.Debug("Making request...");
+            var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit={count}&json=1&tags=sort:random+{default_tags}+{tags_str}"); // Make request
+            _log.Debug("Parsing request...");
+            dynamic response_json = Newtonsoft.Json.JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()); // Parse request
+
+            _log.Debug("Formatting output...");
+            var output = "";
+            foreach (var post in response_json.post)
+            {
+                output += post.file_url.ToString() + "\n";
+            }
+
+            _log.Debug("Replying with files...");
+            await ReplyAsync(output, messageReference: new Discord.MessageReference(Context.Message.Id));
+        }
+        catch
+        {
+            _log.Debug("An exception was thrown. Replying with \"Not found.\"");
+            await ReplyAsync("Not found.");
+        }
     }
 }
