96824e867a7cb82857cd6056dfd4165fe292bdd3
Ibis Sunrosa <drsunrosa@gmail.com>
2022-01-03 01:23:01 -0500

    Default tags and message replies

--- NsfwModule.cs
+++ NsfwModule.cs
@@ -1,5 +1,7 @@
 public class NsfwModule : Discord.Commands.ModuleBase<Discord.Commands.SocketCommandContext>
 {
+    private static string default_tags = "-scat+-loli+-shota";
+
     [Discord.Commands.Command("gelbooru")]
     [Discord.Commands.RequireNsfw]
     public async Task GelbooruAsync([Discord.Commands.Remainder]string tags_str = "")
@@ -7,9 +9,9 @@ public class NsfwModule : Discord.Commands.ModuleBase<Discord.Commands.SocketCom
         tags_str = tags_str.Replace(" ", "+"); // Format tags for API request
 
         var client = new HttpClient(); // Create client for request
-        var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&json=1&tags=sort:random+{tags_str}"); // Make request
+        var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&json=1&tags=sort:random+{default_tags}+{tags_str}"); // Make request
         dynamic response_json = Newtonsoft.Json.JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()); // Parse request
-        await ReplyAsync(response_json.post.file_url.ToString()); // Don't touch this sacred bullshit
+        await ReplyAsync(response_json.post.file_url.ToString(), messageReference: new Discord.MessageReference(Context.Message.Id, Context.Channel.Id, Context.Guild.Id)); // Don't touch this sacred bullshit
     }
 
     [Discord.Commands.Command("mgelbooru")]
@@ -20,7 +22,7 @@ public class NsfwModule : Discord.Commands.ModuleBase<Discord.Commands.SocketCom
         var count = 5;
 
         var client = new HttpClient(); // Create client for request
-        var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit={count}&json=1&tags=sort:random+{tags_str}"); // Make request
+        var response = await client.GetAsync($"https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit={count}&json=1&tags=sort:random+{default_tags}+{tags_str}"); // Make request
         dynamic response_json = Newtonsoft.Json.JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync()); // Parse request
 
         var output = "";
@@ -29,6 +31,6 @@ public class NsfwModule : Discord.Commands.ModuleBase<Discord.Commands.SocketCom
             output += post.file_url.ToString() + "\n";
         }
 
-        await ReplyAsync(output);
+        await ReplyAsync(output, messageReference: new Discord.MessageReference(Context.Message.Id, Context.Channel.Id, Context.Guild.Id));
     }
 }
