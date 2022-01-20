760d4d1b52cf8b9f2414698fd692a459f978675b
Ibis Sunrosa <drsunrosa@gmail.com>
2022-01-20 09:24:01 -0600

    Proper namespacing for config class

--- Config.cs
+++ Config.cs
@@ -1,18 +1,21 @@
-public class Config
+namespace Leirosa
 {
-    public string Token {get; set;}
-    public string SuggestionsPath {get; set;}
-    public string ReportsPath {get; set;}
-    public string VrchatPath {get; set;}
-    public ulong VrchatRoleId {get; set;}
-    public string DefaultGelbooruTags {get; set;}
-    public string Prefix {get; set;}
-    public bool EmbedColorFromUserAvatar {get; set;}
-    public string Status {get; set;}
-    public bool UseCustomStatus {get; set;}
-    public string InviteURL {get; set;}
-    public string SourceURL {get; set;}
-    public string BotName {get; set;}
-    public List<ulong> DeveloperIds {get; set;}
-    public string DatetimeFormat {get; set;}
+    public class Config
+    {
+        public string Token {get; set;}
+        public string SuggestionsPath {get; set;}
+        public string ReportsPath {get; set;}
+        public string VrchatPath {get; set;}
+        public ulong VrchatRoleId {get; set;}
+        public string DefaultGelbooruTags {get; set;}
+        public string Prefix {get; set;}
+        public bool EmbedColorFromUserAvatar {get; set;}
+        public string Status {get; set;}
+        public bool UseCustomStatus {get; set;}
+        public string InviteURL {get; set;}
+        public string SourceURL {get; set;}
+        public string BotName {get; set;}
+        public List<ulong> DeveloperIds {get; set;}
+        public string DatetimeFormat {get; set;}
+    }
 }
