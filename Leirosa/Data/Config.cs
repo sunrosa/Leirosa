5c856242c1b126634e9daf0663904962f1eaed51
Ibis Sunrosa <drsunrosa@gmail.com>
2022-08-03 05:12:28 -0500

    Documentation
    (cherry picked from commit 3bfdf29f60bf5ff09bebb2558f81c21a8323a8dd)

--- Leirosa/Data/Config.cs
+++ Leirosa/Data/Config.cs
@@ -1,114 +1,114 @@
 namespace Leirosa.Data
 {
     /// <summary>
-    /// Leirosa config
+    /// Leirosa config.
     /// </summary>
     public class Config
     {
         /// <summary>
-        /// Discord API token used to log in to the bot
+        /// Discord API token used to log in to the bot.
         /// </summary>
         /// <value></value>
         public string Token {get; set;}
 
         /// <summary>
-        /// The filepath that user suggestions will be written to
+        /// The filepath that user suggestions will be written to.
         /// </summary>
         /// <value></value>
         public string SuggestionsPath {get; set;}
 
         /// <summary>
-        /// The filepath that user error reports will be written to
+        /// The filepath that user error reports will be written to.
         /// </summary>
         /// <value></value>
         public string ReportsPath {get; set;}
 
         /// <summary>
-        /// The filepath that user VRChat login statuses will be written to
+        /// The filepath that user VRChat login statuses will be written to.
         /// </summary>
         /// <value></value>
         public string VRChatPath {get; set;}
 
         /// <summary>
-        /// The Discord role ID that will be applied when logging into VRChat and removed when logging out of VRChat
+        /// The Discord role ID that will be applied when logging into VRChat and removed when logging out of VRChat.
         /// </summary>
         /// <value></value>
         public ulong VRChatRoleId {get; set;}
 
         /// <summary>
-        /// Gelbooru tags that appended to every query
+        /// Gelbooru tags that appended to every query.
         /// </summary>
         /// <value></value>
         public string DefaultGelbooruTags {get; set;}
 
         /// <summary>
-        /// Bot command prefix (used before commands to specify that they're commands)
+        /// Bot command prefix (used before commands to specify that they're commands).
         /// </summary>
         /// <value></value>
         public string Prefix {get; set;}
 
         /// <summary>
-        /// Whether or not to calculate user-targeted embed colors by their avatar (otherwise calculates by their top role)
+        /// Whether or not to calculate user-targeted embed colors by their avatar (otherwise calculates by their top role).
         /// </summary>
         /// <value></value>
         public bool EmbedColorFromUserAvatar {get; set;}
 
         /// <summary>
-        /// Bot status to be displayed on the Discord bot's profile if <see cref="UseCustomStatus"/> is true
+        /// Bot status to be displayed on the Discord bot's profile if <see cref="UseCustomStatus"/> is true.
         /// </summary>
         /// <value></value>
         public string Status {get; set;}
 
         /// <summary>
-        /// Whether or not to display <see cref="Status"/> on the bot's profile
+        /// Whether or not to display <see cref="Status"/> on the bot's profile.
         /// </summary>
         /// <value></value>
         public bool UseCustomStatus {get; set;}
 
         /// <summary>
-        /// OAuth2 URL to invite the bot into Discord guilds
+        /// OAuth2 URL to invite the bot into Discord guilds.
         /// </summary>
         /// <value></value>
         public string InviteURL {get; set;}
 
         /// <summary>
-        /// URL to the bot's source code
+        /// URL to the bot's source code.
         /// </summary>
         /// <value></value>
         public string SourceURL {get; set;}
 
         /// <summary>
-        /// The bot's display name
+        /// The bot's display name.
         /// </summary>
         /// <value></value>
         public string BotName {get; set;}
 
         /// <summary>
-        /// Discord IDs of this bot's developers
+        /// Discord IDs of this bot's developers.
         /// </summary>
         /// <value></value>
         public List<ulong> DeveloperIds {get; set;}
 
         /// <summary>
-        /// Format string used when converting DateTime objects to strings
+        /// Format string used when converting DateTime objects to strings.
         /// </summary>
         /// <value></value>
         public string DatetimeFormat {get; set;}
 
         /// <summary>
-        /// Whether or not to count all command invokations and output them into a json file
+        /// Whether or not to count all command invokations and output them into a json file.
         /// </summary>
         /// <value></value>
         public bool TrackInvokedCommands {get; set;}
 
         /// <summary>
-        /// Path to track command invokations to
+        /// Path to track command invokations to.
         /// </summary>
         /// <value></value>
         public string CommandTrackerPath {get; set;}
 
         /// <summary>
-        /// Log level to be used by the executing assembly (through NLog)
+        /// Log level to be used by the executing assembly (through NLog).
         /// </summary>
         /// <value></value>
         public string LogLevel {get; set;}
