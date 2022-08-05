c4530cbf02d3f25637e1551cc52cefa308a9d8bb
Ibis Sunrosa <drsunrosa@gmail.com>
2022-08-04 20:25:16 -0500

    New log formatting

--- Leirosa/Program.cs
+++ Leirosa/Program.cs
@@ -33,7 +33,7 @@ namespace Leirosa
 
             {
                 var config = new NLog.Config.LoggingConfiguration();
-                var logfile = new NLog.Targets.FileTarget("logfile"){FileName=$"{Config.DataPath}/{Config.LogName}",}; // TODO: Set layout property to include method names in log entries
+                var logfile = new NLog.Targets.FileTarget("logfile"){FileName=$"{Config.DataPath}/{Config.LogName}", Layout="${longdate}|${level}|${callsite}:${callsite-linenumber}|${threadid}|${message}"}; // TODO: Set layout property to include method names in log entries
                 var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
 
                 if (Release)
@@ -109,7 +109,7 @@ namespace Leirosa
                 GatewayIntents = Discord.GatewayIntents.All, // VERY IMPORTANT. SHIT DOESN'T WORK WITHOUT THIS. The Discord API basically neglects to send us shit (most notably SocketGuildUsers), unless we have all intents SPECIFIED in our config.
                 LogGatewayIntentWarnings = false
             });
-            Client.Log += Log;
+            Client.Log += ClientLog;
 
             _log.Debug("Logging in...");
             await Client.LoginAsync(Discord.TokenType.Bot, Config.Token);
@@ -151,7 +151,7 @@ namespace Leirosa
             _isReadied = true;
         }
 
-        private Task Log(Discord.LogMessage msg)
+        private Task ClientLog(Discord.LogMessage msg)
         {
             switch (msg.Severity)
             {
